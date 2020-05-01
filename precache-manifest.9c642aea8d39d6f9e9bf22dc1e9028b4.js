c6fe022b82f8f42391e391e055e3af281d5bb4f2
Sebastian <37258153+PenguinDesign@users.noreply.github.com>
2020-04-30 20:16:51 -0500

    Updates

---
+++ precache-manifest.9c642aea8d39d6f9e9bf22dc1e9028b4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827b9ce84bdef38ecd9d101abb84a3b2",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "4038acbad31f4e7d4d26",
+    "url": "/smart-brain/static/css/2.9dc5248e.chunk.css"
+  },
+  {
+    "revision": "83799a57933247994060",
+    "url": "/smart-brain/static/css/main.152226dc.chunk.css"
+  },
+  {
+    "revision": "4038acbad31f4e7d4d26",
+    "url": "/smart-brain/static/js/2.5143e5e3.chunk.js"
+  },
+  {
+    "revision": "476c15f2ad62ef14880973b98d41458b",
+    "url": "/smart-brain/static/js/2.5143e5e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83799a57933247994060",
+    "url": "/smart-brain/static/js/main.44cf8c25.chunk.js"
+  },
+  {
+    "revision": "8f6ecff7efe8b6405cfe",
+    "url": "/smart-brain/static/js/runtime-main.5c58313c.js"
+  },
+  {
+    "revision": "4d5c6a25e2545253853a74a044164b00",
+    "url": "/smart-brain/static/media/cow.4d5c6a25.svg"
+  },
+  {
+    "revision": "92056c96c916e48b15aa0be0547b48c5",
+    "url": "/smart-brain/static/media/evilsmile.92056c96.svg"
+  }
+]);
